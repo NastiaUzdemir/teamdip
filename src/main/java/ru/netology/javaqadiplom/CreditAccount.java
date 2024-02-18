16d0b787d7ff3796216faacfb7ae3c20cd11a75a
Milena Tabakova <milena-lugowaja@yandex.ru>
2024-02-18 12:20:32 +0300

    exception added

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -23,6 +23,12 @@ public class CreditAccount extends Account {
             );
         }
 
+        if (initialBalance < 0) {
+            throw new IllegalArgumentException(
+                    "Начальный баланс не может быть отрицательным, а у вас: " + initialBalance
+            );
+        }
+
         if (creditLimit < 0) {
             throw new IllegalArgumentException(
                     "Долг банку не должен быть отрицательным, а у вас: " + creditLimit
